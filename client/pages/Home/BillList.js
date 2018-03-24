338dce8f4182449eb22fe30b2283cab3e188a6a2
Patrick Krawczykowski <pkrawz@gmail.com>
2018-03-24 11:29:54 -0500

    styling next-up videos

--- client/pages/Home/BillList.js
+++ client/pages/Home/BillList.js
@@ -20,10 +20,15 @@ const StyledBillList = styled.section`
     display: flex;
     .video {
       margin-right: 2rem;
-      background-color: ${({ theme }) => theme.colors.ui.main};
+      background-size: cover;
+      background-repeat: no-repeat;
+      border: 0.25rem solid transparent;
+      box-shadow: 1rem 1rem 0 0 rgba(243, 126, 90, 0.24);
       height: 7.5rem;
       width: 15rem;
-      box-shadow: 1rem 1rem 0 0 rgba(243, 126, 90, 0.24);
+      &.active {
+        border: 0.25rem solid ${({ theme }) => theme.colors.primary.main};
+      }
     }
   }
 `
@@ -31,7 +36,7 @@ const StyledBillList = styled.section`
 class MainVideo extends React.Component {
   componentDidUpdate = prevProps => {
     if (prevProps.selected !== this.props.selected) {
-      this.node.querySelector("#video").load()
+      this.video.load()
     }
   }
   node = null
@@ -39,11 +44,11 @@ class MainVideo extends React.Component {
     const { bills, selected } = this.props
     const billVideo = bills[selected].fields.video
     return (
-      <div className="main-video" ref={node => (this.node = node)}>
+      <div className="main-video">
         <video
           id="video"
-          controls
-          poster="https://picsum.photos/600/400"
+          ref={node => (this.video = node)}
+          poster={bills[selected].fields.poster.fields.file.url}
           className="bill-video"
         >
           <source src={billVideo.fields.file.url} />
@@ -53,27 +58,31 @@ class MainVideo extends React.Component {
   }
 }
 
-const NextUp = ({ videoIds, changeSelectedVideo }) => (
-  <div className="next-up">
-    {videoIds.map(id => (
-      <div className="video" key={id} onClick={e => changeSelectedVideo(id)} />
-    ))}
-  </div>
-)
-
 class BillList extends React.Component {
   componentDidMount = _ => this.props.getAllBills()
   render() {
-    const { props: { loaded, selectedId, bills, idList } } = this
+    const {
+      props: { loaded, selectedId, bills, idList, changeSelectedVideo }
+    } = this
     if (!loaded) return <Loader />
     return (
       <StyledBillList className="container">
         <p className="subtitle">Track Your Bills</p>
         <MainVideo bills={bills} selected={selectedId} />
-        <NextUp
-          videoIds={idList}
-          changeSelectedVideo={this.props.changeSelectedVideo}
-        />
+        <div className="next-up">
+          {idList.map(id => (
+            <div
+              className={`video ${id === selectedId ? "active" : ""}`}
+              key={id}
+              onClick={e => changeSelectedVideo(id)}
+              style={{
+                backgroundImage: `url(${
+                  bills[id].fields.poster.fields.file.url
+                })`
+              }}
+            />
+          ))}
+        </div>
       </StyledBillList>
     )
   }
