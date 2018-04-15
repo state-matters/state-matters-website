87ffed33d0db6800d773a5223412c3765e369294
Patrick Krawczykowski <pkrawz@gmail.com>
2018-04-15 10:42:47 -0500

    Updated grid to be extendable by classname

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -1,7 +1,10 @@
 import React from "react"
 import styled from "styled-components"
 
-export const Grid = styled.section`
+export const Grid = styled.section.attrs({
+  className: ({ className }) =>
+    className ? `${className} container grid` : "container grid"
+})`
   display: grid;
   grid-template-columns: repeat(${({ columns = 12 }) => columns}, 1fr);
   grid-gap: 2rem;
