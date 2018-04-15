144e78ba62beecb2c4932fe88d8e95e8f08d4988
Patrick Krawczykowski <pkrawz@gmail.com>
2018-04-15 11:15:02 -0500

    allowed container prop on grid and refactored offset

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -2,14 +2,19 @@ import React from "react"
 import styled from "styled-components"
 
 export const Grid = styled.section.attrs({
-  className: ({ className }) =>
-    className ? `${className} container grid` : "container grid"
+  className: ({ className, container }) =>
+    container
+      ? className ? `${className} container grid` : "container grid"
+      : className ? `${className} grid` : "grid"
 })`
   display: grid;
   grid-template-columns: repeat(${({ columns = 12 }) => columns}, 1fr);
   grid-gap: 2rem;
 `
 
-export const Columns = styled.div`
-  grid-column: ${({ span = 12, offset = 1 }) => ` ${offset} / span ${span}`};
+export const Column = styled.div.attrs({
+  offset: ({ offset }) => (offset ? offset + 1 : 1),
+  span: ({ span }) => (span ? span : 12)
+})`
+  grid-column: ${({ span, offset }) => ` ${offset} / span ${span}`};
 `
