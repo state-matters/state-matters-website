771fe447d0810893987621a42e6d0437d2768c38
Patrick Krawczykowski <pkrawz@gmail.com>
2018-05-28 16:19:25 -0500

    Changes how some headers are styled

--- client/baseStyles.js
+++ client/baseStyles.js
@@ -36,20 +36,19 @@ h1, h2, h3, h4 , h5, p {
 
 h1,
 h2,
-h3,
-h4 {
+h3 {
   font-family: "Martel", serif;
 }
 
 h1,
 .hero {
-  font-size: 3rem;
+  font-size: 3.5rem;
   font-weight: 200;
 }
 
 h2,
 .headline {
-  font-size: 2rem;
+  font-size: 3rem;
   font-weight: 300;
 }
 
@@ -75,53 +74,6 @@ h4,
   max-width: 105rem;
   margin: 0 auto;
   padding: 0 2rem;
-}
-
-a {
-  position: relative;
-  display: inline-block;
-  font-weight: 600;
-  color: ${theme.colors.primary["500"]};
-  transition: 200ms;
-  text-decoration: none;
-}
-
-a:hover {
-  color: ${theme.colors.grey["900"]};
-}
-
-a:after {
-  position: absolute;
-  content: "";
-  bottom: 0;
-  left: 6px;
-  height: 6px;
-  width: calc(100% - 6px);
-  background-color: ${theme.colors.primary["500"]};
-  opacity: 0.6;
-  transition: 200ms;
-  z-index: -1;
-}
-
-a:hover:after {
-  height: 100%;
-}
-
-.btn {
-  padding: 1rem 2rem;
-  border: 4px solid ${theme.colors.grey["900"]};
-  border-radius: 2px;
-  background-color: transparent;
-  color: ${theme.colors.grey["900"]};
-  cursor: pointer;
-  font-size: 0.75em;
-  font-weight: 500;
-  transition: 200ms;
-  &.primary {
-    border: 2px solid ${theme.colors.primary["500"]};
-    background-color: ${theme.colors.primary["500"]};
-    color: ${theme.colors.grey["100"]};
-  }
 }
 
 .card {
