fe7b75dba7d4fb0b44cc0ddabb7ef1eda5570fed
Eric Sherman <ericandrewsherman@gmail.com>
2018-06-06 14:52:48 -0500

    placeholder for Articles

--- client/App.js
+++ client/App.js
@@ -6,6 +6,7 @@ import Footer from "components/Footer"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Bill from "./pages/Bills"
+import Articles from "./pages/Articles/index"
 import ArticleShow from "./pages/Articles/Show"
 import Game from "./pages/BillIntro"
 import NotFound from "components/NotFound"
@@ -18,6 +19,7 @@ export default props => (
         <Route path="/about" component={About} />
         <Route path="/bills/:bill_id" component={Bill} />
         <Route path="/articles/:article_id" component={ArticleShow} />
+        <Route path="/articles" component={Articles} />
         <Route path="/bill-intro" component={Game} />
         <Route exact path="/" component={Home} />
         <Route component={NotFound} />
