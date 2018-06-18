401fc514921958c7922ea9b1cd5d4cb9b1745d1c
Patrick Krawczykowski <pkrawz@gmail.com>
2018-06-18 10:57:07 -0500

    use helmet middleware to protect api

--- server.js
+++ server.js
@@ -2,23 +2,19 @@ require("dotenv").config()
 const { join } = require("path")
 const http = require("http")
 const express = require("express")
-const app = express()
 const bodyParser = require("body-parser")
 const logger = require("morgan")
+const helmet = require("helmet")
 
+const app = express()
 const index = require("./api/index")
+const port = process.env.PORT || 8080
 
-app.use(logger("dev"))
+if (proces.env.NODE_ENV !== "production") app.use(logger("dev"))
+app.use(helmet())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(join(__dirname, "public")))
-
-var port = process.env.PORT || 8080
-
-app.set("port", port)
-
-var router = express.Router()
-
 app.use("/api", index)
 
 if (process.env.NODE_ENV === "production") {
