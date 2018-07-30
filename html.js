2f53e8c344e2e3d48842cc624990c8f2b9b79f9b
Patrick Krawczykowski <pkrawz@gmail.com>
2018-07-30 14:03:11 -0500

    fixed favicon in generated html

--- html.js
+++ html.js
@@ -10,17 +10,21 @@ export default ({ title, styles, body }) => `
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
-
     gtag('config', 'UA-112946294-1');
   </script>
   <title>${title}</title>
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
+  <meta name="theme-color" content="#F37E5A">
   <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
   <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
-  <script src="https://d2n4tvy2wsd0oo.cloudfront.net/widget/common/1.3/funraise.min.js"></script>
-  <script src="/bundle.js" defer></script>
-  ${styles}
+  <script async src="https://d2n4tvy2wsd0oo.cloudfront.net/widget/common/1.3/funraise.min.js"></script>
+  <script async defer src="/bundle.js"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Martel:200,300|Poppins:300,400,500,700">
   <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.4.85/css/materialdesignicons.min.css">
+  ${styles}
 </head>
 <body>
 <div id="root">${body}</div>
